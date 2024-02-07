d1751af60e34ebec62076d354e47eba066fedaad
Taycanstar <diminunez06@gmail.com>
2024-02-07 15:35:40 -0500

    Initial Commit

---
+++ pods/podsApp.swift
@@ -0,0 +1,17 @@
+//
+//  podsApp.swift
+//  pods
+//
+//  Created by Dimi Nunez on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct podsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
