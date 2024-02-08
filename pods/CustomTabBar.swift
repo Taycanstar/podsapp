d63d22c247a1106083df917628c190ec484fa87e
Taycanstar <diminunez06@gmail.com>
2024-02-07 21:24:32 -0500

    first commit

---
+++ pods/CustomTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBar.swift
+//  pods
+//
+//  Created by Dimi Nunez on 2/7/24.
+//
+
+import SwiftUI
+
+struct CustomTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBar()
+}
---
+++ pods/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  pods
+//
+//  Created by Dimi Nunez on 2/7/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
