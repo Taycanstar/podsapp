99d666eac8a4638685e85bed725a7fddcd085b5c
Taycanstar <diminunez06@gmail.com>
2024-05-26 00:19:50 -0500

    x

--- pods/ProfileView.swift
+++ pods/ProfileView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import MessageUI
 
 struct ProfileView: View {
     @Environment(\.colorScheme) var colorScheme
     @EnvironmentObject var viewModel: OnboardingViewModel
     @EnvironmentObject var themeManager: ThemeManager
+    @State private var showingMail = false
     
     var body: some View {
         NavigationView {
@@ -38,21 +40,26 @@ struct ProfileView: View {
 
                 }
                 Section(header: Text("Support & About")) {
-                    NavigationLink(destination: NotificationsView()) {
-                        Label("Send Feedback", systemImage: "message")
-                            .foregroundColor(iconColor)
-                    }
-                    NavigationLink(destination: NotificationsView()) {
-                        Label("Terms of Use", systemImage: "doc.plaintext")
-                            .foregroundColor(iconColor)
-                    }
-                    NavigationLink(destination: NotificationsView()) {
-                        Label("Privacy Policy", systemImage: "lock")
-                            .foregroundColor(iconColor)
-                    }
+                    Button(action: {
+                                          self.showingMail = true
+                                      }) {
+                                          Label("Send Feedback", systemImage: "message")
+                                              .foregroundColor(iconColor)
+                                      }
+                    Link(destination: URL(string: "https://www.humuli.com/policies/terms")!) {
+                                          Label("Terms of Use", systemImage: "doc.plaintext")
+                                              .foregroundColor(iconColor)
+                                      }
+                                      Link(destination: URL(string: "https://www.humuli.com/policies/privacy-policy")!) {
+                                          Label("Privacy Policy", systemImage: "lock")
+                                              .foregroundColor(iconColor)
+                                      }
                 }
             }
             .navigationBarTitle("Settings and privacy")
+            .sheet(isPresented: $showingMail) {
+                         MailView()
+                     }
         }
     }
     
@@ -127,4 +134,20 @@ struct PlaybackView: View {
     }
 }
 
+struct MailView: UIViewControllerRepresentable {
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailView>) -> MFMailComposeViewController {
+        let mail = MFMailComposeViewController()
+        mail.setToRecipients(["support@humuli.com"])
+        mail.setSubject("Humuli Feedback")
+        mail.setMessageBody("", isHTML: true)
+        return mail
+    }
+    
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController, context: UIViewControllerRepresentableContext<MailView>) {
+    }
+    
+    static func dismantleUIViewController(_ uiViewController: MFMailComposeViewController, coordinator: ()) {
+        uiViewController.dismiss(animated: true)
+    }
+}
 
