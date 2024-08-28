96e045b1657c894981d1316603f3f96f8907e407
Taycanstar <diminunez06@gmail.com>
2024-08-27 22:19:36 -0500

    logging added

--- pods/Core/Views/PlayerPageView.swift
+++ pods/Core/Views/PlayerPageView.swift
@@ -1,11 +1,11 @@
-////
-////  PlayerPageView.swift
-////  pods
-////
-////  Created by Dimi Nunez on 4/28/24.
 //
+//  PlayerPageView.swift
+//  pods
+//
+//  Created by Dimi Nunez on 4/28/24.
 
-
+//
+//
 //
 //struct PlayerView : View {
 ////    @Binding var videos : [Video]
@@ -182,35 +182,112 @@ struct Player : UIViewControllerRepresentable {
         
     }
 }
+
+//struct PlayerView: View {
+//    var items: [PodItem]
+//    let lifecycleDelegate: ViewLifecycleDelegate?
+//    @Binding var currentIndex: Int
+//    @State private var players: [Int: AVPlayer] = [:]
+//
+//    var body: some View {
+//        VStack(spacing: 0) {
+//            ForEach(items.indices, id: \.self) { index in
+//                let item = items[index]
+//                ZStack {
+//                    if let url = item.videoURL {
+//                        CustomVideoPlayer2(
+//                            url: url,
+//                            player: Binding(
+//                                get: { self.players[index] },
+//                                set: { self.players[index] = $0 }
+//                            ),
+//                            isCurrentVideo: index == currentIndex
+//                        )
+//                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+//                        .offset(y: -30)
+//                        .onTapGesture {
+//                            if let player = players[index] {
+//                                if player.timeControlStatus == .playing {
+//                                    player.pause()
+//                                } else if index == currentIndex {
+//                                    player.play()
+//                                }
+//                            }
+//                        }
+//                    } else {
+//                        PodItemCellImage(item: item)
+//                            .id(item.id)
+//                    }
+//
+//                    VStack {
+//                        Spacer()
+//                        HStack(alignment: .bottom) {
+//                            VStack(alignment: .leading) {
+//                                Text(item.notes)
+//                            }
+//                            .foregroundStyle(.white)
+//                            .font(.body)
+//                            Spacer()
+//                        }
+//                        .padding(.bottom, 80)
+//                    }
+//                    .padding()
+//                }
+//            }
+//        }
+//        .onAppear {
+//            self.lifecycleDelegate?.onAppear()
+//            if let player = players[currentIndex] {
+//                player.play()
+//            }
+//        }
+//        .onDisappear {
+//            self.lifecycleDelegate?.onDisappear()
+//            for player in players.values {
+//                player.pause()
+//            }
+//        }
+//        .onChange(of: currentIndex) { oldIndex, newIndex in
+//            players[oldIndex]?.pause()
+//            players[newIndex]?.seek(to: .zero)
+//            players[newIndex]?.play()
+//        }
+//        .padding(.bottom, 80)
+//    }
+//}
+
 struct PlayerView: View {
-    @Environment(\.presentationMode) var presentationMode
     var items: [PodItem]
     let lifecycleDelegate: ViewLifecycleDelegate?
-    @State private var players: [Int: AVPlayer] = [:]
     @Binding var currentIndex: Int
-    
+    @State private var players: [Int: AVPlayer] = [:]
+
     var body: some View {
         VStack(spacing: 0) {
             ForEach(items.indices, id: \.self) { index in
                 let item = items[index]
                 ZStack {
+                    Color.black.edgesIgnoringSafeArea(.all)
+                    
                     if let url = item.videoURL {
-                        CustomVideoPlayer2(
-                            url: url,
-                            player: Binding(
-                                get: { self.players[index] },
-                                set: { self.players[index] = $0 }
-                            ),
-                            isCurrentVideo: index == currentIndex
-                        )
-                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-                        .offset(y: -30)
-                        .onTapGesture {
-                            if let player = players[index] {
-                                if player.timeControlStatus == .playing {
-                                    player.pause()
-                                } else if index == currentIndex {
-                                    player.play()
+                        GeometryReader { geometry in
+                            CustomVideoPlayer2(
+                                url: url,
+                                player: Binding(
+                                    get: { self.players[index] },
+                                    set: { self.players[index] = $0 }
+                                ),
+                                isCurrentVideo: index == currentIndex
+                            )
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: geometry.size.width, height: geometry.size.height)
+                            .onTapGesture {
+                                if let player = players[index] {
+                                    if player.timeControlStatus == .playing {
+                                        player.pause()
+                                    } else if index == currentIndex {
+                                        player.play()
+                                    }
                                 }
                             }
                         }
@@ -218,7 +295,7 @@ struct PlayerView: View {
                         PodItemCellImage(item: item)
                             .id(item.id)
                     }
-                    
+
                     VStack {
                         Spacer()
                         HStack(alignment: .bottom) {
@@ -233,8 +310,10 @@ struct PlayerView: View {
                     }
                     .padding()
                 }
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
             }
         }
+        .background(Color.black)
         .onAppear {
             self.lifecycleDelegate?.onAppear()
             if let player = players[currentIndex] {
@@ -248,91 +327,127 @@ struct PlayerView: View {
             }
         }
         .onChange(of: currentIndex) { oldIndex, newIndex in
-            for (index, player) in players {
-                if index == newIndex {
-                    player.seek(to: .zero)
-                    player.play()
-                } else {
-                    player.pause()
-                }
-            }
+            players[oldIndex]?.pause()
+            players[newIndex]?.seek(to: .zero)
+            players[newIndex]?.play()
         }
-        .padding(.bottom, 80)
     }
 }
+
+//struct PlayerPageView: UIViewRepresentable {
+//    var items: [PodItem]
+//    @Binding var currentIndex: Int
+//
+//    func makeCoordinator() -> Coordinator {
+//        return Coordinator(parent: self)
+//    }
+//
+//    func makeUIView(context: Context) -> UIScrollView {
+//        let view = UIScrollView()
+//        
+//        let childView = UIHostingController(rootView: PlayerView(items: self.items, lifecycleDelegate: context.coordinator, currentIndex: $currentIndex))
+//        childView.view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * CGFloat(items.count))
+//        
+//        let tabBarHeight: CGFloat = 55
+//        let totalHeight = (UIScreen.main.bounds.height + tabBarHeight) * CGFloat(items.count)
+//        view.contentSize = CGSize(width: UIScreen.main.bounds.width, height: totalHeight)
+//
+//        view.addSubview(childView.view)
+//        view.showsVerticalScrollIndicator = false
+//        view.showsHorizontalScrollIndicator = false
+//        view.contentInsetAdjustmentBehavior = .never
+//        view.isPagingEnabled = true
+//        view.delegate = context.coordinator
+//
+//        view.bounces = false
+//        context.coordinator.setupInitialVideo(view: view)
+//        return view
+//    }
+//
+//    func updateUIView(_ uiView: UIScrollView, context: Context) {
+//        let tabBarHeight: CGFloat = 55
+//        uiView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: (UIScreen.main.bounds.height + tabBarHeight) * CGFloat(items.count))
+//
+//        for i in 0..<uiView.subviews.count {
+//            uiView.subviews[i].frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * CGFloat(items.count))
+//        }
+//    }
+//
+//    class Coordinator: NSObject, UIScrollViewDelegate, ViewLifecycleDelegate {
+//        var parent: PlayerPageView
+//        var index = 0
+//
+//        init(parent: PlayerPageView) {
+//            self.parent = parent
+//            self.index = parent.currentIndex
+//        }
+//
+//        func setupInitialVideo(view: UIScrollView) {
+//            let yOffset = CGFloat(parent.currentIndex) * UIScreen.main.bounds.height
+//            view.setContentOffset(CGPoint(x: 0, y: yOffset), animated: false)
+//        }
+//
+//        func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+//            let currentIndex = Int(scrollView.contentOffset.y / UIScreen.main.bounds.height)
+//            parent.currentIndex = currentIndex
+//        }
 
 struct PlayerPageView: UIViewRepresentable {
     var items: [PodItem]
     @Binding var currentIndex: Int
-    
+
     func makeCoordinator() -> Coordinator {
         return Coordinator(parent: self)
     }
-    
+
     func makeUIView(context: Context) -> UIScrollView {
         let view = UIScrollView()
         
         let childView = UIHostingController(rootView: PlayerView(items: self.items, lifecycleDelegate: context.coordinator, currentIndex: $currentIndex))
         childView.view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * CGFloat(items.count))
-        let tabBarHeight: CGFloat = 55
-        let totalHeight = (UIScreen.main.bounds.height + tabBarHeight) * CGFloat(items.count)
-        view.contentSize = CGSize(width: UIScreen.main.bounds.width, height: totalHeight)
         
+        view.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * CGFloat(items.count))
+
         view.addSubview(childView.view)
         view.showsVerticalScrollIndicator = false
         view.showsHorizontalScrollIndicator = false
         view.contentInsetAdjustmentBehavior = .never
         view.isPagingEnabled = true
         view.delegate = context.coordinator
-        
+
         view.bounces = false
         context.coordinator.setupInitialVideo(view: view)
         return view
     }
-    
+
     func updateUIView(_ uiView: UIScrollView, context: Context) {
-        let tabBarHeight: CGFloat = 55
-        uiView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: (UIScreen.main.bounds.height + tabBarHeight) * CGFloat(items.count))
-        
+        uiView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * CGFloat(items.count))
+
         for i in 0..<uiView.subviews.count {
             uiView.subviews[i].frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * CGFloat(items.count))
         }
     }
-    
+
     class Coordinator: NSObject, UIScrollViewDelegate, ViewLifecycleDelegate {
         var parent: PlayerPageView
         var index = 0
-        
+
         init(parent: PlayerPageView) {
             self.parent = parent
             self.index = parent.currentIndex
         }
-        
+
         func setupInitialVideo(view: UIScrollView) {
             let yOffset = CGFloat(parent.currentIndex) * UIScreen.main.bounds.height
             view.setContentOffset(CGPoint(x: 0, y: yOffset), animated: false)
         }
-        
+
         func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
             let currentIndex = Int(scrollView.contentOffset.y / UIScreen.main.bounds.height)
             parent.currentIndex = currentIndex
-            
-            if index != currentIndex {
-                // Pause the previous video
-                if index >= 0 && index < parent.items.count {
-                    parent.items[index].player?.pause()
-                }
-                
-                index = currentIndex
-                parent.currentIndex = index
-                
-                // Play the current video
-                if currentIndex >= 0 && currentIndex < parent.items.count {
-                    parent.items[currentIndex].player?.play()
-                }
-            }
         }
-        
+
+
         func onAppear() {
             // Play the initial video
             if index >= 0 && index < parent.items.count {
@@ -346,5 +461,7 @@ struct PlayerPageView: UIViewRepresentable {
                 item.player?.pause()
             }
         }
+   
     }
 }
+
