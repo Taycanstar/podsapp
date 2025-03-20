694b974f97f4462229193c2351f6aa4b2633ab4e
Taycanstar <diminunez06@gmail.com>
2025-03-20 16:20:48 -0500

    xex

---
+++ pods/Core/Components/ButtonWithIcon.swift
@@ -0,0 +1,34 @@
+//
+//  ButtonWithIcon.swift
+//  Pods
+//
+//  Created by Dimi Nunez on 3/20/25.
+//
+
+import SwiftUI
+struct ButtonWithIcon: View {
+    let label: String
+    let iconName: String
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            HStack(spacing: 16) {
+                Image(systemName: iconName)
+                    .font(.system(size: 24))
+                    .foregroundColor(.accentColor)
+                Text(label)
+                    .font(.system(size: 16))
+                    .foregroundColor(.accentColor)
+                Spacer()
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 12)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color(UIColor.secondarySystemBackground))
+            .cornerRadius(12)
+        }
+        .padding(.horizontal)
+        .padding(.top)
+    }
+}
