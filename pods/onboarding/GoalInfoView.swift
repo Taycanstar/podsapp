0b51f3e1d4b261d8f61c571f5c44e21f54253385
Taycanstar <diminunez06@gmail.com>
2025-04-18 15:26:44 -0500

    mads

---
+++ pods/onboarding/GoalInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  GoalInfoView.swift
+//  Pods
+//
+//  Created by Dimi Nunez on 4/18/25.
+//
+
+import SwiftUI
+
+struct GoalInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalInfoView()
+}
