0ff8de924d3de25899298fa7379665199fc44ec2
Taycanstar <diminunez06@gmail.com>
2025-04-19 13:51:18 -0500

    mads

---
+++ pods/onboarding/AccomplishView.swift
@@ -0,0 +1,18 @@
+//
+//  AccomplishView.swift
+//  Pods
+//
+//  Created by Dimi Nunez on 4/19/25.
+//
+
+import SwiftUI
+
+struct AccomplishView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccomplishView()
+}
