0b3b5b716dcfd308f4d626bb26033b4763f40c45
Taycanstar <diminunez06@gmail.com>
2025-04-20 09:10:57 -0500

    mads

---
+++ pods/onboarding/CreatingPlanView.swift
@@ -0,0 +1,18 @@
+//
+//  CreatingPlanView.swift
+//  Pods
+//
+//  Created by Dimi Nunez on 4/19/25.
+//
+
+import SwiftUI
+
+struct CreatingPlanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatingPlanView()
+}
