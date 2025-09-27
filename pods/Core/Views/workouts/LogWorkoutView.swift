4596b3280fccc59dd2ecf90db38dec051ed1d5f8
Taycanstar <diminunez06@gmail.com>
2025-09-27 14:16:37 -0500

    sf

--- pods/Core/Views/workouts/LogWorkoutView.swift
+++ pods/Core/Views/workouts/LogWorkoutView.swift
@@ -329,7 +329,7 @@ struct LogWorkoutView: View {
                     .padding(.bottom, 12)
             } else {
                 Color("primarybg")
-                    .frame(height: 0)
+                    .frame(height: 1)
                     .frame(maxWidth: .infinity)
                     .accessibilityHidden(true)
             }
